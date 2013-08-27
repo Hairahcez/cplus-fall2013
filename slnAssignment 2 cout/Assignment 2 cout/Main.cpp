584550c64981d7e1bfd9b63060be8b0965e64a3b
Hairahcez <zstue@yahoo.com>
2013-08-27 11:02:48 -0500

    Assignment 2 cout

---
+++ slnAssignment 2 cout/Assignment 2 cout/Main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+
+
+int main()
+{
+	// Write a string.
+	cout << "Computers, computers everywhere";
+	// Write a string on a new line with a tab.
+	cout << "\n\tas far as I can C.";
+	// Write a string on a new line.
+	cout << "\nI really, really like these things.";
+	// Write a string on a new line with a tab.
+	cout << "\n\tOh joy.  Oh joy for me!";
+
+
+
+	_getch();
+	return 0;
+}
